4209cdf977bb749c1fa70a05c503711f81d34fcc
jrwaka <j.rwaka@alustudent.com>
2024-07-01 04:41:59 -0700

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,12 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
